8d95956bf65a5369bab3cbdab24f25bcb4e280de
Hongzehua <hzh9867811@outlook.com>
2024-06-16 21:38:24 +0800

    feat: 动态面包屑方案处理

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -2,6 +2,8 @@
   <div class="navbar">
     <!-- 汉堡 -->
     <hamburger class="hamburger-container"></hamburger>
+    <!-- 面包屑 -->
+    <breadcrumb class="breadcrumb-container"></breadcrumb>
     <div class="right-menu">
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
@@ -34,6 +36,7 @@
 import {} from 'vue'
 import { useStore } from 'vuex'
 import Hamburger from '@/components/Hamburger'
+import Breadcrumb from '@/components/Breadcrumb'
 
 const store = useStore()
 /**
@@ -63,6 +66,10 @@ const logout = () => {
     &:hover {
       background: rgba(0, 0, 0, 0.1);
     }
+  }
+
+  .breadcrumb-container {
+    float: left;
   }
 
   .right-menu {
