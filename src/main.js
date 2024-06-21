247a83b98a2e0a6b8a82272afe1258c304acfeff
Hongzehua <hzh9867811@outlook.com>
2024-06-21 19:11:41 +0800

    feat: 用户列表分页展示

--- src/main.js
+++ src/main.js
@@ -9,10 +9,12 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './styles/index.scss'
 import installIcons from '@/icons'
 import './permission'
+import insallFilter from '@/filters'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 installIcons(app)
+insallFilter(app)
 app.use(store).use(router).use(ElementPlus).use(i18n).mount('#app')
