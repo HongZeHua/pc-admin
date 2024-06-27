941eadc40f5cbe3b93f6f97095ad4266f709c8a4
Hongzehua <hzh9867811@outlook.com>
2024-06-27 16:17:51 +0800

    feat: 富文本与markdown综合处理之创建文章

--- src/views/article-detail/index.vue
+++ src/views/article-detail/index.vue
@@ -18,8 +18,8 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { ref, onActivated } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import { articleDetail } from '@/api/article'
 
 // 获取文章数据
@@ -31,8 +31,13 @@ const getArticleDetail = async () => {
 }
 getArticleDetail()
 
+onActivated(getArticleDetail)
+
 // 编辑
-const onEditClick = () => {}
+const router = useRouter()
+const onEditClick = () => {
+  router.push(`/article/editor/${articleId}`)
+}
 </script>
 
 <style lang="scss" scoped>
