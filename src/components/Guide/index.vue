f02bbb3815e3778a288bc0770631475059465fa1
Hongzehua <hzh9867811@outlook.com>
2024-06-27 16:40:11 +0800

    fix: guide（导航功能）实现国际化

--- src/components/Guide/index.vue
+++ src/components/Guide/index.vue
@@ -12,11 +12,12 @@ import { driver } from 'driver.js'
 import 'driver.js/dist/driver.css'
 import { useI18n } from 'vue-i18n'
 import steps from './steps'
+import { watchSwitchLang } from '@/utils/i18n'
 
 const i18n = useI18n()
 
 let driverObj = null
-onMounted(() => {
+const initDriver = () => {
   driverObj = driver({
     showProgress: true,
     // 点击蒙版关闭
@@ -26,10 +27,16 @@ onMounted(() => {
     prevBtnText: i18n.t('msg.guide.prev'),
     steps: steps(i18n)
   })
+}
+onMounted(() => {
+  initDriver()
 })
 const onClick = () => {
   driverObj.drive()
 }
+
+// 语言切换时，重新初始化 driver
+watchSwitchLang(initDriver)
 </script>
 
 <style scoped lang="scss"></style>
