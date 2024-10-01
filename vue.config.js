6bd452f73e1740769d75c3dfe09a97462469183f
Hongzehua <hzh9867811@outlook.com>
2024-10-01 17:59:17 +0800

    perf: 使用external配合cdn在生产环境下由cdn引入部分体积过大的外部库

--- vue.config.js
+++ vue.config.js
@@ -21,6 +21,29 @@ module.exports = defineConfig({
     }
   },
   chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      const cdn = {
+        css: [
+          'https://cdn.bootcdn.net/ajax/libs/element-plus/2.7.5/index.min.css',
+          'https://uicdn.toast.com/editor/3.0.2/toastui-editor.min.css'
+        ],
+        js: [
+          'https://cdn.bootcdn.net/ajax/libs/element-plus/2.7.5/index.full.min.js',
+          'https://cdn.bootcdn.net/ajax/libs/element-plus-icons-vue/2.3.1/index.min.js',
+          'https://cdn.jsdelivr.net/npm/wangeditor@latest/dist/wangEditor.min.js',
+          'https://uicdn.toast.com/editor/3.0.2/toastui-editor-all.min.js',
+          'https://cdn.bootcdn.net/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+          'https://cdn.bootcdn.net/ajax/libs/vue/3.2.13/vue.global.min.js',
+          'https://cdn.bootcdn.net/ajax/libs/vue-i18n/9.0.0/vue-i18n.cjs.min.js',
+          'https://cdn.bootcdn.net/ajax/libs/axios/1.7.2/axios.min.js'
+        ]
+      }
+      config.plugin('html').tap((args) => {
+        args[0].cdn = cdn
+        return args
+      })
+    }
+
     config.plugin('define').tap((definitions) => {
       Object.assign(definitions[0], {
         __VUE_OPTIONS_API__: 'true',
@@ -42,5 +65,22 @@ module.exports = defineConfig({
       })
       .end()
     config.resolve.alias.set('path', require.resolve('path-browserify'))
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.devtool = false
+      config.externals = {
+        'element-plus': 'ElementPlus',
+        '@element-plus/icons-vue': 'ElementPlusIconsVue',
+        wangeditor: 'E',
+        '@toast-ui/editor': 'MkEditor',
+        'xlsx/xlsx.mjs': 'XLSX',
+        vue: 'createApp',
+        'vue-i18n': 'useI18n',
+        axios: 'axios'
+      }
+    } else {
+      config.devtool = 'eval-cheap-module-source-map'
+    }
   }
 })
